8ac06af06b062f5f5561baef1d2c5926f3bf0785
Nightmare1984 <gunther6000@gmail.com>
2025-03-22 08:38:05 -0400

    Adding Flashlight Function And Adding Fov Changer For walk To Sprint And Back To Walk

--- Scripts/First Person Player Script/player.gd
+++ Scripts/First Person Player Script/player.gd
@@ -6,6 +6,8 @@ extends CharacterBody3D
 @onready var standing_collision = $"standing collision"
 @onready var crouching_collision = $"crouching collision"
 @onready var ray_cast_3d = $RayCast3D
+@onready var spot_light_3d: SpotLight3D = $head/SpotLight3D
+@onready var camera_3d: Camera3D = $head/Camera3D
 
 #Speed Vars
 
@@ -25,6 +27,11 @@ var direction = Vector3.ZERO
 @export var jump_velocity = 4.5
 @export var crouching_depth = -0.5
 
+#Fov Vars
+@export var fov_default = 65.0
+@export var fov_sprint = 85.0
+var current_fov = fov_default
+
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
@@ -39,32 +46,30 @@ func _input(event):
 
 
 func _physics_process(delta):
-	
+	if Input.is_action_just_pressed("flashlight"):
+		spot_light_3d.visible = !spot_light_3d.visible
 	#HandelMovementState
-	
 	if Input.is_action_pressed("crouch"):
 		current_speed = crouching_speed
 		head.position.y = lerp(head.position.y,0.7 + crouching_depth,delta*lerp_speed)
-		standing_collision.disabled = true
-		crouching_collision.disabled = false
-		
+		standing_collision.disabled = true	
 	elif !ray_cast_3d.is_colliding():
 		standing_collision.disabled = false
 		crouching_collision.disabled = true
 		head.position.y = lerp(head.position.y,0.7,delta*lerp_speed)
 		if Input.is_action_pressed("sprint"):
 			current_speed = sprinting_speed
+			current_fov = fov_sprint
 		else:
 			current_speed = walking_speed
-	
+			current_fov = fov_default
+	camera_3d.fov = lerp(camera_3d.fov, current_fov, delta * lerp_speed)
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y -= gravity * delta
-
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = jump_velocity
-
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir = Input.get_vector("left", "right", "forward", "backward")
@@ -76,5 +81,4 @@ func _physics_process(delta):
 	else:
 		velocity.x = move_toward(velocity.x, 0, current_speed)
 		velocity.z = move_toward(velocity.z, 0, current_speed)
-
 	move_and_slide()
